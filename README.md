4464d227f36953c0fc8e4740e05cb28ef520650b
Felix Bartels <felix@host-consultants.de>
2020-04-17 08:01:02 +0200

    Add scripting to setup oidc login for Kopano WebApp
    Include description on getting going
    
    Signed-off-by: Felix Bartels <f.bartels@kopano.com>

---
+++ README.md
@@ -0,0 +1,52 @@
+# Configuration helper for OIDC login for Kopano WebApp
+
+This project will configure Kopano WebApp so that it will use OpenID Connect (OIDC) for logging in. This means that once a user has logged into WebApp he can seamlessly use other applications that are configured to use the OIDC Provider of Univention (like for example Kopano Meet).
+
+## Requirements
+
+To checkout the project `git` needs to be installed on the system. Further more the script makes the assumption that the "Kopano Core", "Kopano WebApp" (needs to be at least version 3.5.14.2539) and "OpenID Connect Provider" apps are installed on the same system.
+
+```bash
+univention-install git
+```
+
+After `git` has been installed it needs to be cloned to the local disk by running the below command and afterwards switch into the newly created directory.
+
+TODO: move repo to a more official looking location. Where?
+```bash
+$ git clone https://stash.kopano.io/scm/~fbartels/ucs-oidc.git
+$ cd ucs-oidc
+$ ls
+LICENSE.txt  README.md  docker-compose.yml  run.sh
+```
+
+## WebApp configuration notice
+
+To ensure compatibility with the configuration listener of Kopano4UCS the script converts all quoting in WebApps `config.php` to double quotes. This procedure should be safe, but a backup of the old file is kept in case this causes any issues with the current installation. For new installations `config.php` will use double quotes by default [starting with Kopano WebApp 4.0](https://forum.kopano.io/topic/3070/webapp-config-php-double-quotes-consistency).
+
+## Running the script
+
+To configure your Kopano Core and the Kopano WebApp app for login via OpenID Connect just run the script once. Running the script will take care of configuring the OpenID Connect Provider App, create a helper user in Kopano to let Konnect establish a permenent session, configure the Univention webserver and make the neccesary config changes.
+
+Running the script will also create a file called `.env` in the current directory. In this file all installation specific variables are stored. In case the FQDN should be changed, or the OpenID Connect Provider app is available under a different domain just change these values here and rerun the script.
+
+Running the script should only take a few moments. After it has completed it will print the URL from which you can reach Kopano WebApp.
+
+## Things you should know
+
+- The OpenID Connect flow is sensitive to the domain name its called with. FQDN_KONNECT should reflect both the domain that Konnect and WebApp are reachable under.
+- When enabling OIDC login for WebApp the old login mechanism is no longer available.
+- When configuring OIDC login for kopano-server then kopano-server needs to resolve the oidc discovery document at startup (which means Konnect must already be running).
+- /etc/ssl is mounted into the Konnect container, so all ssl certificates trusted by the host are also trusted by Konnect.
+
+## Known issues
+
+- Logging out of Kopano WebApp (and therefore the internal Konnect instance) will not log the user out of the Univention OpenID Provider
+- When opening from an url other than $FQDN_KONNECT there will be redirection to the OpenID Provider. To circumvent this from happening every access from a different domain than the expected should be rewritten.
+  - Example:
+  ```
+  RewriteCond %{REQUEST_URI} ^/webapp$ [OR]
+  RewriteCond %{REQUEST_URI} ^/webapp/
+  RewriteCond %{HTTP_HOST} !^$FQDN_KONNECT$ [NC]
+  RewriteRule ^(.*)$ https://$FQDN_KONNECT/webapp/ [R,L]
+```
---
+++ run.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+err_report() {
+    echo "An error occurded on line $1 of this script."
+}
+
+trap 'err_report $LINENO' ERR
+
+random_string() {
+    LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c32
+}
+
+# install git to make the version lookup succeed
+dpkg -s git 2>/dev/null >/dev/null || univention-install git
+
+if [ "$(ucr get appcenter/apps/kopano-core/status)" != "installed" ]; then
+    echo "Kopano Core is not installed"
+    exit 1
+fi
+
+if [ "$(ucr get appcenter/apps/kopano-webapp/status)" != "installed" ]; then
+    echo "Kopano WebApp is not installed"
+    exit 1
+fi
+
+if [ "$(ucr get appcenter/apps/openid-connect-provider/status)" != "installed" ]; then
+    echo "OpenID Connect Provider is not installed"
+    exit 1
+fi
+
+eval "$(ucr shell)"
+
+if [ ! -e ./.env ]; then
+	cat <<-EOF >"./.env"
+INSECURE=false
+FQDN_KONNECT=$hostname.$domainname
+FQDN_SSO=$(ucr get ucs/server/sso/fqdn)
+clientsecret=$(random_string)
+OIDCLOGIN=$(random_string)
+EOF
+fi
+
+source .env
+
+# Create helper user to establish a global session for Konnect (required for external auth provider usage)
+udm users/user create "$@" --ignore_exists \
+    --position "cn=kopano,"$ldap_base"" \
+    --set "username"="oidc-helper" \
+    --set "description"="Kopano OpenID Connect Helper" \
+    --set "password"="$OIDCLOGIN" \
+    --set "lastname"="OIDC Helper" \
+    --set mailPrimaryAddress="oidc@$(ucr get domainname)" \
+	--set kopano-role=user \
+    --set kopano-user-hidden=1
+
+# Sync user list to create the new user
+kopano-admin --sync
+
+# remove oidc registration so that it can be readded with potentially a new fqdn
+# will print a "E: object not found" to the log if entry is not there
+udm oidc/rpservice remove "$@" --dn cn=kopano-core,cn=oidc,cn=univention,"$(ucr get ldap/base)" || true
+
+# add oidc registration. ucs will take care of restarting the app
+udm oidc/rpservice create "$@" --ignore_exists \
+ --set name=kopano-core \
+ --position=cn=oidc,cn=univention,"$(ucr get ldap/base)" \
+ --set clientid=kopano-webapp \
+ --set clientsecret="$clientsecret" \
+ --set trusted=yes \
+ --set applicationtype=web \
+ --set redirectURI="https://$FQDN_KONNECT/kopanoid/signin/v1/identifier/oauth2/cb"
+
+# apache2 conf
+cat << EOF >/etc/apache2/ucs-sites.conf.d/kopano-konnect.conf
+ProxyPass /kopanoid/.well-known/openid-configuration http://127.0.0.1:38777/.well-known/openid-configuration retry=0
+ProxyPass /kopanoid/ http://127.0.0.1:38777/kopanoid/ retry=0
+EOF
+
+invoke-rc.d apache2 reload
+
+# Fix quoting in config.php so that the listener can properly update it
+cp /etc/kopano/webapp/config.php{,-bak}
+tr "'" '"' </etc/kopano/webapp/config.php >/etc/kopano/webapp/config.php-quotes
+mv /etc/kopano/webapp/config.php-quotes /etc/kopano/webapp/config.php
+
+# Set config options in WebApp and kopano-core for oidc
+ucr set \
+	kopano/webapp/config/LANG?'"en_US"' \
+	kopano/webapp/config/OIDC_ISS="\"https://$FQDN_KONNECT/kopanoid/"\" \
+	kopano/webapp/config/OIDC_CLIENT_ID='"Kopano-WebApp"' \
+	kopano/cfg/server/kcoidc_issuer_identifier="https://$FQDN_KONNECT/kopanoid/" \
+	kopano/cfg/server/enable_sso=yes \
+	kopano/cfg/server/kcoidc_initialize_timeout?360
+
+# run the following to undo:
+# ucr unset kopano/webapp/config/OIDC_ISS kopano/webapp/config/OIDC_CLIENT_ID kopano/cfg/server/kcoidc_issuer_identifier kopano/cfg/server/enable_sso && systemctl restart kopano-server
+# pull containers before starting
+docker-compose pull
+
+# restart kopano-server to apply changes
+systemctl restart kopano-server
+
+# start containers
+docker-compose up -d
+
+echo "Please go to https://$FQDN_KONNECT/webapp to log into Kopano WebApp via oidc."
