bd2f6a6cfc24ce48c87e843037cbbbb322c54cc2
Felix Bartels <felix@host-consultants.de>
2020-04-21 08:59:10 +0200

    ask for fqdn
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- run.sh
+++ run.sh
@@ -47,10 +47,18 @@ fi
 eval "$(ucr shell)"
 
 if [ ! -e ./.env ]; then
+	value_default="$hostname.$domainname"
+	read -r -p "FQDN where kopano-server is running [$value_default]: " new_value
+	FQDN_KONNECT=${new_value:-$value_default}
+
+	value_default="$(ucr get ucs/server/sso/fqdn)"
+	read -r -p "FQDN where the openid connect provider is running [$value_default]: " new_value
+	FQDN_SSO=${new_value:-$value_default}
+
 	cat <<-EOF >"./.env"
 INSECURE=false
-FQDN_KONNECT=$hostname.$domainname
-FQDN_SSO=$(ucr get ucs/server/sso/fqdn)
+FQDN_KONNECT=$FQDN_KONNECT
+FQDN_SSO=$FQDN_SSO
 clientsecret=$(random_string)
 OIDCLOGIN=$(random_string)
 EOF
@@ -58,7 +66,8 @@ fi
 
 source .env
 
-# Create helper user to establish a global session for Konnect (required for external auth provider usage)
+echo "Creating helper user to establish a global session for Konnect"
+# required for external auth provider usage
 udm users/user create "$@" --ignore_exists \
     --position cn=kopano,"$ldap_base" \
     --set username="oidc-helper" \
@@ -73,7 +82,8 @@ udm users/user create "$@" --ignore_exists \
 # will print a "E: object not found" to the log if entry is not there
 udm oidc/rpservice remove "$@" --dn cn=kopano-core,cn=oidc,cn=univention,"$(ucr get ldap/base)" || true
 
-# add oidc registration. ucs will take care of restarting the app
+echo "adding oidc registration"
+# ucs will take care of restarting the app
 udm oidc/rpservice create "$@" --ignore_exists \
     --set name=kopano-core \
     --position=cn=oidc,cn=univention,"$(ucr get ldap/base)" \
@@ -83,7 +93,7 @@ udm oidc/rpservice create "$@" --ignore_exists \
     --set applicationtype=web \
     --set redirectURI="https://$FQDN_KONNECT/kopanoid/signin/v1/identifier/oauth2/cb"
 
-# apache2 conf
+echo "configuring Apache"
 cat << EOF >/etc/apache2/ucs-sites.conf.d/kopano-konnect.conf
 ProxyPass /kopanoid/.well-known/openid-configuration http://127.0.0.1:38777/.well-known/openid-configuration retry=0
 ProxyPass /kopanoid/ http://127.0.0.1:38777/kopanoid/ retry=0
@@ -96,7 +106,7 @@ cp /etc/kopano/webapp/config.php{,-bak}
 tr "'" '"' </etc/kopano/webapp/config.php >/etc/kopano/webapp/config.php-quotes
 mv /etc/kopano/webapp/config.php-quotes /etc/kopano/webapp/config.php
 
-# Set config options in WebApp and kopano-core for oidc
+echo "setting config options in WebApp and kopano-core for oidc"
 ucr set \
     kopano/webapp/config/LANG?'"en_US"' \
     kopano/webapp/config/OIDC_ISS="\"https://$FQDN_KONNECT/kopanoid/"\" \
@@ -107,19 +117,19 @@ ucr set \
 
 # run the following to undo:
 # ucr unset kopano/webapp/config/OIDC_ISS kopano/webapp/config/OIDC_CLIENT_ID kopano/cfg/server/kcoidc_issuer_identifier kopano/cfg/server/enable_sso && systemctl restart kopano-server
-# pull containers before starting
+echo "pulling containers before starting"
 docker-compose pull
 
-# start containers
+echo "starting containers"
 docker-compose up -d
 
-echo "restart kopano-server to apply changes"
+echo "restarting kopano-server to apply changes"
 systemctl restart kopano-server
 
-echo "Please go to https://$FQDN_KONNECT/webapp to log into Kopano WebApp via oidc."
-
-echp "Waiting for kopano-server to startup, then syncing user list to create the new user"
+echo "Waiting for kopano-server to startup, then syncing user list to create the new user"
 dockerize \
         -wait tcp://127.0.0.1:236 \
         -timeout 120s \
 	kopano-admin --sync
+
+echo "Please go to https://$FQDN_KONNECT/webapp to log into Kopano WebApp via oidc."
